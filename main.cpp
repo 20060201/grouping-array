1d1279ae0c809de9d498e8c8ab8afc1b58a3176c
Mira <a750387@stdmail.nssh.ntpc.edu.tw>
2020-11-10 14:54:29 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <algorithm>
+#include<string>
+using namespace std; 
+
+int main() {
+    int c, n; 
+    cin >> c >> n; //參加人數 & 每組幾人
+    
+    int a[c];
+    for (int i = 0; i < c; i++) {
+        cin >> a[i]; //戰力值
+    }
+
+}
