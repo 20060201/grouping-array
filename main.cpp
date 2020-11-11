b001104dc94d201c2a747add8e91563bbee6db1a
Mira <a750387@stdmail.nssh.ntpc.edu.tw>
2020-11-11 13:48:15 +0000

    001

--- main.cpp
+++ main.cpp
@@ -12,4 +12,28 @@ int main() {
         cin >> a[i]; //戰力值
     }
 
+    int maxTeam = -1;
+    int maxPower = -1;
+    int totalPower = 0;
+
+    int j = 0;
+    int totalTeam = 0;
+
+    for (int i = c - 1; i >= 0; i--) {
+        j ++;
+        totalPower += a[i];
+
+        if (j == n || i == 0) {
+            totalTeam ++;
+            if (totalPower >= maxPower) {
+                maxPower = totalPower;
+                maxTeam = totalTeam;
+            }
+
+            j = 0;
+            totalPower = 0;
+        }
+    } 
+
+    cout << maxTeam << " " << maxPower;
 }
